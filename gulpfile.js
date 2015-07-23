f258dd37df014d8e1b84a6aec5ce34a3799327a5
Sam Brannon <sam@evolvelabs.com>
2015-07-22 22:40:28 -0500

    Added values to themes

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
 gulp.task('watch', function(){
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['watch']);
