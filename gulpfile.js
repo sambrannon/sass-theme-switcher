c40416b77ca97141b69c3ca2cc91784687610c31
Sam Brannon <sam@evolvelabs.com>
2015-07-22 21:56:40 -0500

    Added empty theme files. Upgraded gulp sass. Added branberg website markup.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
+gulp.task('watch', function(){
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['watch']);
